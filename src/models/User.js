a410ca93011c948a1b6ee512e59dbfde9f7cf287
Mateus de Aquino <86917791+mateusaquinomr@users.noreply.github.com>
2026-06-09 16:30:54 -0300

    fix: versão sem async/await para o Render

--- src/models/User.js
+++ src/models/User.js
@@ -9,17 +9,22 @@ const userSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now },
 });
 
-userSchema.pre('save', async function (next) {
-    try {
-        if (!this.isModified('password')) {
-            return next();
-        }
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    if (!user.isModified('password')) {
+        return next();
     }
+
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
